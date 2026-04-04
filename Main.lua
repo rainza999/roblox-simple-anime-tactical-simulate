4c8019748d2f7b464e0b06bf0c20251633048e2e
Rain <rain.patchanok@gmail.com>
2026-04-04 22:54:58 +0700

    x

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
-warn("### RAID + BOSS MAIN BUILD V3 ###")
+warn("### RAID + BOSS MAIN BUILD V4 ###")
 
 local BASE = "https://raw.githubusercontent.com/rainza999/roblox-simple-anime-tactical-simulate/main/"
 local ts = tostring(os.time())
